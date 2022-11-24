422ad40b23a0c934657fe3ee4ae0b8e0ed47ab6a
Natnael16 <natnaeldenbi@gmail.com>
2022-11-24 11:11:41 +0300

    use chromium

--- src/app.ts
+++ src/app.ts
@@ -98,7 +98,7 @@ app.delete("/delete-pdf/:regNo", async (req, res) => {
 const generate = async (body) => {
   try {
     const browser = await puppeteer.launch({
-      product: "firefox"
+      args: ['--no-sandbox', '--disable-setuid-sandbox'] 
     });
 
     // Create a new page
